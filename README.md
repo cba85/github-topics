a4f78412ddbdb102295a862196a3598cf02c5c80
Clément Barbaza <cba85@users.noreply.github.com>
2025-02-27 10:36:44 +0100

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Github topics
+
+Auto complete topics on a Github repository that uses node.js and npm (`package.json` file).
+
+## Install
+
+```sh
+$ npm install
+```
+
+## Usage
+
+Set a `GITHUB_AUTH_TOKEN` environment variable that contains your Github authentication token.
+
+```sh
+$ node main.js <repository>
+# E.g.
+$ node main.js cba85/github-topics
+```
---
+++ main.js
@@ -0,0 +1,85 @@
+import { Octokit } from "@octokit/rest";
+
+// CLI
+
+if (process.argv.length != 3) {
+  console.error("Please enter a repository name");
+  process.exit(1);
+}
+
+const repositoryName = process.argv[2];
+
+// Check repository name validity
+
+const githubRepositoryRegex = /[a-z0-9.\_\-]+\/[a-z0-9.\_\-]+/g;
+
+const isRepositoryNameValid = githubRepositoryRegex.test(repositoryName);
+
+if (!isRepositoryNameValid) {
+  console.error("Please enter a valid repository name (username/name)");
+  process.exit(1);
+}
+
+console.log(`> Update ${repositoryName} topics`);
+
+// Github API
+
+const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN });
+
+// Get repository and its topics
+
+let repository;
+
+try {
+  const response = await octokit.rest.repos.get(`GET /repos/${repositoryName}`);
+  repository = response.data;
+} catch (error) {
+  console.error(error.response.data.message);
+  process.exit(1);
+}
+
+const topics = repository.topics;
+
+// Get package.json file content
+
+let packageJson;
+
+try {
+  const response = await octokit.request(`GET /repos/${repositoryName}/contents/package.json`);
+  packageJson = response.data;
+} catch (error) {
+  console.error(error.response.data.message);
+  process.exit(1);
+}
+
+const bufferObj = Buffer.from(packageJson.content, "base64");
+const packageContent = bufferObj.toString("utf8");
+
+const json = JSON.parse(packageContent);
+
+// Get package.json dependencies
+
+if (!json.dependencies) {
+  console.error("No dependencies in package.json file");
+  process.exit(1);
+}
+
+const packageDependencies = Object.keys(json.dependencies);
+
+// Combinate Github repository topics and package.json dependencies
+
+const dependencies = Array.from(new Set([...packageDependencies, ...topics]));
+
+// Update repository topics on Github
+
+try {
+  await octokit.request(`PUT /repos/${repositoryName}/topics`, {
+    names: dependencies,
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
+
+console.log(`> Found ${JSON.stringify(dependencies)}`);
+console.log(`> Topics have been updated on Github repository`);
